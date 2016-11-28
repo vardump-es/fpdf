176119b998aca513542c7c339f2043ccd19ff790
Pablo <pablo@anoopsystems.com>
2016-11-28 17:27:15 +0100

    no message

--- export.php
+++ export.php
@@ -3,22 +3,27 @@
 include_once "vendor/fpdf/fpdf.php";
 
 //para utilizar FPDF debemos extender su clase principal
-class ExportPDF extends FPDF{
+class ExportadorPDF extends FPDF{
 
 	private $clientes;
 
 	const limitePagina = 240; 
-	const comienzoTabla = 40;
+	const comienzoTabla = 30;
+	const alturaFila = 10;
 
 	//meter los clientes que vamos a exportar
 	public function setClientes($clientes){
 		$this->clientes = $clientes;
 	}
 
+	//exportar PDF
 	public function exportar(){
 
+		//añadir primer pagina
+		$this->AddPage();
+
 		//se recorre a los clientes
-		foreach ($clientes as $cliente) {
+		foreach ($this->clientes as $cliente) {
 			$this->exportarCliente($cliente);
 			//se verifica si se necesita o no una nueva página
 			$this->analizarSaltoDePagina();
@@ -30,14 +35,48 @@ class ExportPDF extends FPDF{
 	
 	private function analizarSaltoDePagina(){
 		//si la posición X del documento supera el límite de salto de pagina
-		if($this->starty>self::limitePagina){
+		if($this->GetY()>self::limitePagina - self::alturaFila){
 			//se añade una nueva página
 			$this->AddPage();	
 		}
 	}
 
+
+	//dibujar cada fila de cliente
+	public function exportarCliente($cliente){
+
+		$starty = $this->GetY();
+		//formato de la fuente (fuente,estilo,tamaño)
+		$this->SetFont('Arial','',10);
+
+		//columna identificador cliente
+		//se posiciona el cursor
+		$this->setXY(10,$starty);
+		//se convierde de UTF-8 a windows-1252 porque FPDF no acepta UTF-8
+		$this->Cell(20,self::alturaFila, iconv('UTF-8', 'windows-1252', $cliente->id_cliente),'T','C');
+
+		//columna nombre
+		//se posiciona el cursor
+		$this->setXY(30,$starty);
+		//se hace un cuadro de texto 
+		$this->Cell(100,self::alturaFila, iconv('UTF-8', 'windows-1252', $cliente->nombre),'T','L');
+
+		//columna edad
+		//se posiciona el cursor
+		$this->setXY(130,$starty);
+		//se hace un cuadro de texto 
+		$this->Cell(20,self::alturaFila, iconv('UTF-8', 'windows-1252', $cliente->edad) ,'T','C');
+
+		//salto de línea
+		$this->Ln();
+
+	}
+
 	//sobrecargamos la funcion AddPage para que cada vez que se agrega una página
 	public function AddPage(){
+		//llamamos a la creación de paginas de FPDF y luego añadimos nuestra funcinalidad
+		parent::addPage();
+
 		//se dibuja la tabla
 		$this->dibujarTabla();
 		//se posiciona en el comienzo de la tabla
@@ -47,13 +86,17 @@ class ExportPDF extends FPDF{
 	//funcion que dibuja la tabla en cuestión
 	private function dibujarTabla(){
 
+
+		//formato de la fuente (fuente,estilo,tamaño)
+		$this->SetFont('Arial','B',10);
+
 		//columna identificador cliente
 		//se posiciona el cursor
 		$this->setXY(10,self::comienzoTabla);
 		//se hace un cuadro de texto (ancho, alto, texto, bordes, alineación del texto)
 		$this->MultiCell(20,5,'ID',1,'C');
-		// se dibuja la columna
-		$this->Rect(10,self::comienzoTabla,30,self::limitePagina);
+		// se dibuja la columna (x,y,ancho,alto)
+		$this->Rect(10,self::comienzoTabla,20,self::limitePagina-self::comienzoTabla);
 
 		//columna nombre
 		//se posiciona el cursor
@@ -61,15 +104,23 @@ class ExportPDF extends FPDF{
 		//se hace un cuadro de texto 
 		$this->MultiCell(100,5,'Nombre',1,'C');
 		// se dibuja la columna
-		$this->Rect(30,self::comienzoTabla,30,self::limitePagina)
+		$this->Rect(30,self::comienzoTabla,100,self::limitePagina-self::comienzoTabla);
 
 		//columna edad
 		//se posiciona el cursor
 		$this->setXY(130,self::comienzoTabla);
 		//se hace un cuadro de texto 
-		$this->MultiCell(10,5,'Edad',1,'C');
+		$this->MultiCell(20,5,'Edad',1,'C');
 		// se dibuja la columna
-		$this->Rect(30,self::comienzoTabla,30,self::limitePagina)
+		$this->Rect(130,self::comienzoTabla,20,self::limitePagina-self::comienzoTabla);
+	}
+
+	//sobrecargar la funcion Header para imprimir un encabezado
+
+	public function Header(){
+		$this->SetFont('Arial','B',16);
+		$this->setXY(10,10);
+		$this->Cell(200,10, 'PRUEBA SALIDA',0,'C');
 	}
 
 }
--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $exportador = new ExportadorPDF();
 
 //obtener clientes
 $clientes = json_decode(file_get_contents("data/clientes.json"));
+
 //agregar datos al exportador
 $exportador->setClientes($clientes);
 
